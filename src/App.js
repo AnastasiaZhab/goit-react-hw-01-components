a0f8eeed67435417edbedff1faaa0db7aee9dd09
Anastasia Zhaboiedova <tsyfrak@gmail.com>
2022-02-04 23:54:07 +0200

    создает разметку 1й таски

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
+import Profile from './components/Profile/Profile'
+import Statistics from './components/Statistics/Statistics'
 import './App.css';
+import user from '../src/user.json'
+import data from '../src/data.json'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+        <Statistics
+        title={data}
+        stats={data}
+        />
     </div>
+  
   );
 }
 
