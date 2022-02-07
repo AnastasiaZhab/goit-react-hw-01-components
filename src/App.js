5fa9c316faa8b1f223aee8a9328369662bd25948
Anastasia Zhaboiedova <tsyfrak@gmail.com>
2022-02-07 14:28:12 +0200

    создает js код для 4й таски

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Profile from './components/Profile/Profile'
 import Statistics from './components/Statistics/Statistics'
 import FriendList from './components/FriendList/Friendlist';
+import TransactionHistory from './components/Transaction/TransactionHistory';
 import './App.css';
 import user from './data/user.json'
 import data from './data/data.json'
 import friends from './data/friends.json'
+import transactions from './data/transactions.json'
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
       />
         <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />;
+      <TransactionHistory transactions={transactions} />;
         
     </div>
   
